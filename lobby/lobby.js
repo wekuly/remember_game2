132ce47270a9455254e386bffa2ee8dbe4c53a82
wekuly <wekuly@naver.com>
2026-02-03 22:02:09 +0900

    Updates

--- lobby/lobby.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * 로비 모듈 (클라이언트/공용)
- * - 매칭 요청·취소·매칭 완료 이벤트 타입
- * - 실제 큐·매칭 로직은 서버 lobby 모듈
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=lobby.js.map
